47599b26a65e4000abb0ef8984f18b955992f219
Mehdi <selmimehdi970@gmail.com>
2026-04-14 19:56:58 +0100

    add file hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="bg-primary text-white">
+      <Navbar />
+      <Hero/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
-
+import React from "react";
+import { motion } from "motion/react";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaGithub } from "react-icons/fa";
 const Navbar = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div className="flex justify-between items-center  p-10">
+        <GiHamburgerMenu className="text-2xl cursor-pointer font-bold bg-white  text-neutral w-9 p-1.5 h-9 rounded-full" />
+        <motion.p
+          initial={{ opacity: 0, x: 20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          className="font-semibold text-xl"
+        >
+          Mehdi Selmi
+        </motion.p>
+        <div className="flex gap-2 text-2xl">
+          <a href="#https://www.linkedin.com/in/mehdi-selmi-939a82273/">
+            <FaLinkedinIn />
+          </a>
+          <a href="#https://github.com/mehdiselmi">
+            <FaGithub />
+          </a>
 
-export default Navbar
+          <a href="#https://www.facebook.com/mehdi.selmi.121">
+            <FaFacebookF />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
